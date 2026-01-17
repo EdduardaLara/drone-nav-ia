1bca7ebc244c8e035b867eaa7636bab03736666a
Duda <146853931+EdduardaLara@users.noreply.github.com>
2026-01-16 21:25:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
-from PIL import Image
-import numpy as np
-import tensorflow as tf
-
-app = FastAPI()
-
-# Carrega o modelo .h5
-modelo = tf.keras.models.load_model("modelo_Coffee_melhorado.h5")
-
-# Defina suas classes (ajuste conforme seu dataset)
-CLASSES = ["Cercospora", "Phoma", "Leaf Miner", "Leaf Rust", "Healthy"]
-
-def preprocess(img: Image.Image):
-    img = img.resize((128, 128))  # ajuste para sua resolução
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
-    return img
-
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    # abre a imagem
-    img = Image.open(file.file).convert("RGB")
-    data = preprocess(img)
-    
-    # previsao
-    pred = modelo.predict(data)
-    classe = CLASSES[np.argmax(pred)]
-    conf = float(np.max(pred))
+    from PIL import Image
+    import numpy as np
 
-    return JSONResponse({
-        "classe": classe,
-        "confianca": round(conf, 3)
-    })
+    img = Image.open(file.file).convert("RGB")
+    img = img.resize((64, 64))
+    arr = np.array(img) / 255.0
+    arr = np.expand_dims(arr, axis=0)
+
+    pred = modelo.predict(arr)[0]
+
+    classes = ['COFFEE']   # se só tem 1 classe no treinamento
+
+    return {
+        "raw_prediction": pred.tolist(),
+        "classe_predita": classes[int(np.argmax(pred))]
+    }
